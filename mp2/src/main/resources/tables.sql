387ee04a817b390b378b9412463eac4d104e74cc
rkspivey <rkspivey@gmail.com>
2016-01-26 21:40:48 -0500

    Changed code to store ontime percentage in cassandra instead of count.

--- mp2/src/main/resources/tables.sql
+++ mp2/src/main/resources/tables.sql
@@ -18,16 +18,16 @@ create table "airport_dest_ontime" (
 	PRIMARY KEY (("origin"), "ontime_percentage", "destination")
 ) with clustering order by ("ontime_percentage" desc);
 
-create table "source_dest_carrier_ontime_arrivals" (
+create table "source_dest_carrier_ontime" (
 	"origin" text,
 	"destination" text,
 	"airline_id" text,
-	"ontime_arrivals" int,
+	"ontime_percentage" double,
 	"origin_name" text,
 	"destination_name" text,
 	"airline_name" text,
-	PRIMARY KEY ("origin", "destination", "airline_id", "ontime_arrivals")
-);
+	PRIMARY KEY (("origin", "destination"), "ontime_percentage", "airline_id")
+) with clustering order by ("ontime_percentage" desc);
 
 create table "connecting_flights" (
 	"flight_num1" int,
